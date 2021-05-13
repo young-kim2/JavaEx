8b606fdf4d342e18d09f180c40a12eb1df17eaa6
김영 <dhehd215@daum.net>
2021-05-13 22:46:05 +0900

    객체 필드와 객체 못보낸것

--- JavaEx/src/com/javaex/oop/goods/v2/GoodsApp.java
+++ JavaEx/src/com/javaex/oop/goods/v2/GoodsApp.java
@@ -3,7 +3,15 @@ package com.javaex.oop.goods.v2;
 public class GoodsApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Goods notebook=new Goods();
+		notebook.name="LG Gram";
+		notebook.price=1500000;
+		
+		Goods smartphone=new Goods();
+		smartphone.name="iPhoneSE";
+		smartphone.price=650000;
+		System.out.printf("%s, %d원%n", notebook.name,notebook.price);
+		System.out.printf("%s, %d원%n", smartphone.name,smartphone.price);
 
 	}
 
