ce72917be1d54e8de82ed64501e86de158a5047c
김영 <dhehd215@daum.net>
2021-05-15 14:05:23 +0900

    중간 Commit

--- JavaEx/src/com/javaex/oop/goods/v2/GoodsApp.java
+++ JavaEx/src/com/javaex/oop/goods/v2/GoodsApp.java
@@ -4,14 +4,18 @@ public class GoodsApp {
 
 	public static void main(String[] args) {
 		Goods notebook=new Goods();
-		notebook.name="LG Gram";
-		notebook.price=1500000;
+		
+		//setter를 이용한 우회 접근
+		notebook.setName("LG Gram");
+		notebook.setPrice(1500000);
 		
 		Goods smartphone=new Goods();
-		smartphone.name="iPhoneSE";
-		smartphone.price=650000;
-		System.out.printf("%s, %d원%n", notebook.name,notebook.price);
-		System.out.printf("%s, %d원%n", smartphone.name,smartphone.price);
+		smartphone.setName("iPhoneSE");
+		smartphone.setPrice(650000);
+		System.out.printf("%s, %d원%n", notebook.getName(),notebook.getPrice());
+		notebook.showInfo();
+		System.out.printf("%s, %d원%n", smartphone.getName(),smartphone.getPrice());
+		smartphone.showInfo();
 
 	}
 
