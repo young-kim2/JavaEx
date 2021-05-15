9ddf317fdfbadcc8afd606581a8fbdaac52ec01f
김영 <dhehd215@daum.net>
2021-05-15 15:37:33 +0900

    객체 기본

---
+++ JavaEx/src/com/javaex/oop/goods/v4/GoodsApp.java
@@ -0,0 +1,23 @@
+package com.javaex.oop.goods.v4;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+		Goods notebook=new Goods("LG Gram",1500000); //사용자 정의 생성자 활용
+		Goods smartphone=new Goods("iphone SE",650000);
+		
+		notebook.showInfo();
+		//누군가 악의적, 실수로
+		/*
+		 notebook.setPrice(15000);
+		 notebook.showInfo();
+		 */
+		System.out.printf("%s, %d원%n",notebook.getName(),notebook.getPrice());
+		notebook.showInfo();
+		
+		System.out.printf("%s, %d원%n", smartphone.getName(),smartphone.getPrice());
+		smartphone.showInfo();
+
+	}
+
+}
---
+++ JavaEx/src/com/javaex/oop/staticmember/StaticEx.java
@@ -0,0 +1,39 @@
+package com.javaex.oop.staticmember;
+
+public class StaticEx {
+	//클래스 변수(static)
+	//:모든 인스턴스에서 공유
+	//:static ->instance(X)
+	//:instance->static(O)
+	public static int refCount;
+	public static String classVar;
+	//인스턴스 변수
+	//:개별 객체 내부에서만 접근
+	public String instanceVar;
+	
+	//static 영역 초기화는 static 블록에서
+	//클래스가 로드될때 단 한번 실행
+	static {
+		refCount=0;
+		classVar="클래스 변수";
+		System.out.println("=>Static Block");
+		//instanceVar="인스턴스 변수";
+		//static->instance 멤버(x)
+	}
+	//생성자
+	public StaticEx() {
+		refCount++;
+		//instance->static(O)
+		System.out.println("=>Instance 생성");
+		System.out.println("=>참조 Count:"+refCount);
+	}
+//소멸자
+	@Override
+	protected void finalize() throws Throwable {
+		refCount--;
+		System.out.println("소멸자 호출");
+		super.finalize();
+	}
+	
+
+}
