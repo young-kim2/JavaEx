c7f75f2cc948929a51034789ad8f883209361cc4
김영 <dhehd215@daum.net>
2021-05-21 16:21:36 +0900

    Generics

---
+++ JavaEx/src/com/javaex/api/generics/v1/ObjectBox.java
@@ -0,0 +1,16 @@
+package com.javaex.api.generics.v1;
+
+public class ObjectBox {
+	private Object content;
+
+	public Object getContent() {
+		return content;
+	}
+
+	public void setContent(Object content) {
+		this.content = content;
+	}
+	
+	
+
+}
---
+++ JavaEx/src/com/javaex/api/generics/v3/ObjectBox.java
@@ -0,0 +1,16 @@
+package com.javaex.api.generics.v3;
+
+public class ObjectBox {
+	private Object content;
+
+	public Object getContent() {
+		return content;
+	}
+
+	public void setContent(Object content) {
+		this.content = content;
+	}
+	
+	
+
+}
