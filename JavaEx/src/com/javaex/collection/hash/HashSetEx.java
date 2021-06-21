8c58d1b4fe33437e07fcddd219c0f83eb5812a70
김영 <dhehd215@daum.net>
2021-06-21 10:38:29 +0900

    Custom 객체 HashSet 사용

--- JavaEx/src/com/javaex/collection/hash/HashSetEx.java
+++ JavaEx/src/com/javaex/collection/hash/HashSetEx.java
@@ -5,7 +5,8 @@ import java.util.HashSet;
 public class HashSetEx {
 
 	public static void main(String[] args) {
-		basicHashSetEx();
+		//basicHashSetEx();
+		  customHashSetEx();
 
 	}
 	private static void basicHashSetEx() {
@@ -38,5 +39,31 @@ public class HashSetEx {
 		hs.clear();
 		System.out.println("HashSet:"+hs);
 	}
+	
+	private static void customHashSetEx() {
+		HashSet<Student> hs=new HashSet<>();
+		
+		Student s1= new Student("홍길동",10);
+		Student s2= new Student("장길산",20);
+		Student s3= new Student("전우치",30);
+		
+		hs.add(s1);
+		hs.add(s2);
+		hs.add(s3);
+		
+		System.out.println("학생부:"+hs);
+		
+		Student s4 = new Student("홍길동",10);
+		hs.add(s4);
+		System.out.println("hashcode of s1:"+s1.hashCode());
+		System.out.println("hashcode of s4:"+s4.hashCode());
+		System.out.println("s1 equals s4 ?"+s1.equals(s4));
+		
+		//포함 여부 확인
+		boolean exists=hs.contains(new Student("홍길동",10));
+		System.out.println("홍길동(10) exists:"+exists);
+		
+	}
+	
 
 }
