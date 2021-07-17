935b5efada01bd192ac9133b08014833fc09982f
김영 <dhehd215@daum.net>
2021-07-17 15:01:33 +0900

    PreparedStatement

--- JavaEx/src/com/javaex/jdbc/HRSearchEmployeesPstmt.java
+++ JavaEx/src/com/javaex/jdbc/HRSearchEmployeesPstmt.java
@@ -1,13 +1,15 @@
 package com.javaex.jdbc;
 import java.sql.*;
 import java.util.Scanner;
-public class HRSearchEmployees {
+public class HRSearchEmployeesPstmt {
 
 	public static void main(String[] args) {
 		//클래스 Scanner를 사용하여 사원 이름을 입력 받아 사원 정보를 검색(부분 검색)하는 프로그램을 작성해 봅시다
+		//PreparedStatement를 이용한 실행 계획의 준비
 		String dburl="jdbc:oracle:thin:@localhost:1521:xe";
 		Connection conn=null;
-		Statement stmt=null;
+		//Statement stmt=null;
+		PreparedStatement pstmt=null;
 		ResultSet rs=null;
 		
 		Scanner scanner=new Scanner(System.in);
@@ -21,6 +23,7 @@ public class HRSearchEmployees {
 			String keyword=scanner.next();
 			
 			//SQL
+			/*
 			String sql="SELECT first_name, last_name, "+
 			           "email, phone_number, hire_date "+
 					   "FROM employees "+
@@ -31,8 +34,20 @@ public class HRSearchEmployees {
 			
 			stmt=conn.createStatement();
 			rs=stmt.executeQuery(sql);
+			*/
+			//동적으로 데이터 연결할 공간을 ?로 비워 둔다
+			String sql="SELECT first_name, last_name, "+
+			           "email, phone_number, hire_date FROM employees "+
+					   "WHERE lower(first_name) LIKE ? OR lower(last_name) LIKE?";
+			pstmt=conn.prepareStatement(sql);
+			//실행 계획을 준비해 둔다
+			//동적 파라미터 설정
 			
-			while(rs.next()) {
+			pstmt.setString(1,"%"+keyword+"%");
+			pstmt.setString(2,"%"+keyword+"%");
+			//쿼리 수행
+			rs=pstmt.executeQuery();
+			while(rs.next()){
 				System.out.printf("%s %s: %s, %s, %s%n", rs.getString(1),rs.getString(2),//이름
 						          rs.getString(2),rs.getString("phone_number"),
 						          rs.getString("phone_number"),
@@ -45,7 +60,7 @@ public class HRSearchEmployees {
 				}finally {
 					try {
 						rs.close();
-						stmt.close();
+						pstmt.close();
 						conn.close();
 					}catch(Exception e) {
 						e.printStackTrace();
