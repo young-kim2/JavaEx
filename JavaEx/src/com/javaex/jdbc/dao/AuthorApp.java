1eb11ff3628ac2694c361a0c4575decd478c01b8
김영 <dhehd215@daum.net>
2021-07-17 17:50:01 +0900

    Dao Pattern 연습

---
+++ JavaEx/src/com/javaex/jdbc/dao/AuthorApp.java
@@ -0,0 +1,74 @@
+package com.javaex.jdbc.dao;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+public class AuthorApp {
+
+	public static void main(String[] args) {
+		listAuthors();
+		insertAuthor();
+		deleteAuthor();
+		updateAuthor();
+		listAuthors();
+
+	}
+	private static void listAuthors() {
+		AuthorDao dao=new AuthorDaoOracleImpl();
+		List<AuthorVo> list=dao.getList();
+		
+		//Iterator(반복자) 추출
+		Iterator<AuthorVo> it=list.iterator();
+		while(it.hasNext()) {
+			//내용이 더 있으면
+			AuthorVo vo=it.next();
+			//내용 불러오기
+			System.out.printf("%d, %s, %s%n",vo.getId(), vo.getName(), vo.getBio());
+		}
+	}
+	private static void insertAuthor() {
+		//Scanner로 이름, bio 정보를 입력받아 ->DAO에서 INSERT
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("이름:");
+		String name=scanner.next();
+		System.out.print("경력:");
+		String bio=scanner.next();
+		
+		AuthorVo vo=new AuthorVo(name,bio);
+		AuthorDao dao=new AuthorDaoOracleImpl();
+		boolean success=dao.insert(vo);
+		
+		System.out.println("Author INSERT:"+(success?"성공":"실패"));
+		scanner.close();
+	}
+	private static void deleteAuthor() {
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("삭제할 작가:");
+		long authorId=scanner.nextLong();
+		AuthorDao dao=new AuthorDaoOracleImpl();
+		
+		boolean success=dao.delete(authorId);
+		
+		System.out.println("Author DELETE:"+(success?"성공":"실패"));
+		scanner.close();
+	}
+	private static void updateAuthor() {
+		//작가 PK확인
+		//업데이트 내용:name,bio
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("업데이트할 작가:");
+		long authorId=scanner.nextLong();
+		System.out.print("변경할 이름:");
+		String name=scanner.next();
+		
+		System.out.print("변경할 이력:");
+		String bio=scanner.next();
+		AuthorVo vo=new AuthorVo(authorId,name,bio);
+		AuthorDao dao=new AuthorDaoOracleImpl();
+		
+		boolean success=dao.update(vo);
+		
+		System.out.println("Author UPDATE:"+(success?"성공":"실패"));
+		scanner.close();
+	}
+
+}
---
+++ JavaEx/src/com/javaex/jdbc/dao/AuthorDaoOracleImpl.java
@@ -0,0 +1,118 @@
+package com.javaex.jdbc.dao;
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.*;
+
+public class AuthorDaoOracleImpl implements AuthorDao {
+    //공통 코드인 접속 코드 로직을 분리합시다.
+	private Connection getConnection() throws SQLException{
+		Connection conn=null;
+		try {
+			//드라이버 로드
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String dburl="jdbc:oracle:thin:@localhost:1521:xe";
+			conn=DriverManager.getConnection(dburl,"C##KIMY","1234");
+			
+		}catch(ClassNotFoundException e) {
+			System.err.println("드라이버 로드 실패!");
+		}
+		return conn;
+	}
+	@Override
+	public List<AuthorVo> getList(){
+		//자바 객체를 이용 주고 받기 :SQL RS-> Java 객체로 변환
+		List<AuthorVo>list=new ArrayList<>();
+		Connection conn=null;
+		Statement stmt=null;
+		ResultSet rs=null;
+		
+		try {
+			conn=getConnection();
+			stmt=conn.createStatement();
+			
+			String sql="SELECT id, name, bio FROM author";
+			rs=stmt.executeQuery(sql);
+			
+			//루프를 돌면서 자바 객체로 변환
+			while(rs.next()) {
+				Long id=rs.getLong("id");
+				String name=rs.getString("name");
+				String bio=rs.getString("bio");
+				
+				//VO객체 생성
+				AuthorVo vo=new AuthorVo(id, name, bio);
+				list.add(vo);
+				
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	@Override
+	public boolean insert(AuthorVo vo){
+		//매개변수 vo를 전달받아서 author테이블에 insert
+		//INSERT, UPDATE, DELETE 수행시 executeUpdate 메서드-> 결과 int(영향 받은 레코드 수)
+		Connection conn=null;
+		String sql="INSERT INTO author VALUES(seq_author.NEXTVAL,?,?)";
+		//실행계획
+		int insertedCount=0;
+        try {
+        	conn=getConnection();
+        	PreparedStatement pstmt=conn.prepareStatement(sql);
+        	//파라미터 연결
+        	pstmt.setString(1,vo.getName());
+        	pstmt.setString(2, vo.getBio());
+        	
+        	insertedCount=pstmt.executeUpdate();
+        }catch(SQLException e) {
+        	e.printStackTrace();
+        }
+        return insertedCount==1;
+        //INSERT 문장의 성공 여부
+	}
+	@Override
+	public boolean update(AuthorVo vo) {
+		//VO객체 전달 name, bio 필드를 교체
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		
+		int updatedCount=0;
+		
+		try {
+			conn=getConnection();
+			String sql="UPDATE author SET name=?, bio=? WHERE id=?";
+			            pstmt=conn.prepareStatement(sql);
+			            pstmt.setString(1, vo.getName());
+			            pstmt.setString(2, vo.getBio());
+			            pstmt.setLong(3, vo.getId());
+			            
+			            updatedCount=pstmt.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return updatedCount==1;
+	}
+	@Override
+	public boolean delete(Long id) {
+		Connection conn=null;
+		
+		PreparedStatement pstmt=null;
+		int deletedCount=0;
+		try {
+			conn=getConnection();
+			
+			String sql="DELETE FROM author WHERE id=?";
+			pstmt=conn.prepareStatement(sql);
+			
+			pstmt.setLong(1,id);
+			
+			deletedCount=pstmt.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return deletedCount==1;
+	}
+	
+
+}
